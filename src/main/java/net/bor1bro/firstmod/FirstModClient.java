e13758a988efaead052861f9612a7ce57145d58d
bor1bro <133193063+bor1bro@users.noreply.github.com>
2023-12-20 18:19:10 +0300

    Initial commit

---
+++ src/main/java/net/bor1bro/firstmod/FirstModClient.java
@@ -0,0 +1,11 @@
+package net.bor1bro.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
